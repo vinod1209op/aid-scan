22a8529f921513a1b0a827bf14ae11eb2e6d9e71
vinod1209op <62650750+vinod1209op@users.noreply.github.com>
2025-04-23 06:46:54 -0700

    Update train_transformer.py

--- models/transformer/train_transformer.py
+++ models/transformer/train_transformer.py
@@ -1,5 +1,4 @@
 from transformers import RobertaTokenizer, RobertaModel
-import torch
 
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
 model = RobertaModel.from_pretrained("microsoft/codebert-base")
